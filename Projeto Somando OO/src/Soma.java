9ddaf5e67fdcefaa0a0d09104ed1aeccb6e04504
Lev_TheDev <levinosecretti@outlook.com>
2023-07-28 12:51:17 -0400

    Adicionando pastas de projetos com swing.

---
+++ Projeto Somando OO/src/Soma.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rogerlevino
+ */
+public class Soma {
+    // Atributos
+    double v1, v2, total;
+    
+    // Métodos
+    public void calcularSoma(){
+        total = v1 + v2;
+    }
+    
+}
